970d6fd1cec61a8f15ad9e889de00f61e6b40c76
Enidikay <abdallah.yakidine@gmail.com>
2026-06-24 16:25:20 +0200

    exo 4

---
+++ src/main/java/utils/TestMethodeStatic.java
@@ -0,0 +1,16 @@
+package main.java.utils;
+
+public class TestMethodeStatic {
+
+    public static void main(String[] args) {
+        String chaine = "12";
+        int a = 10;
+        int b = 24;
+
+        int nombre = Integer.parseInt(chaine);
+
+        System.out.println(nombre);
+
+        System.out.println(Integer.max(a,b));
+    }
+}
