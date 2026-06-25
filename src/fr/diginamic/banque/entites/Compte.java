906e608c5fe19e1497b436700f323fa90991a194
Enidikay <abdallah.yakidine@gmail.com>
2026-06-25 10:10:51 +0200

    tp 5

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,35 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+    private int numCompte;
+    private double soldeCompte;
+
+    public Compte(int numCompte, double soldeCompte) {
+        this.numCompte = numCompte;
+        this.soldeCompte = soldeCompte;
+    }
+
+    public int getNumCompte() {
+        return numCompte;
+    }
+
+    public void setNumCompte(int numCompte) {
+        this.numCompte = numCompte;
+    }
+
+    public double getSoldeCompte() {
+        return soldeCompte;
+    }
+
+    public void setSoldeCompte(double soldeCompte) {
+        this.soldeCompte = soldeCompte;
+    }
+
+    @Override
+    public String toString() {
+        return "Compte{" +
+                "numCompte=" + numCompte +
+                ", soldeCompte=" + soldeCompte +
+                '}';
+    }
+}
---
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,8 @@
+package fr.diginamic.banque.entites;
+
+public class TestBanque {
+    static void main(String[] args) {
+        Compte compte1 = new Compte(5508940, 5253.509);
+        System.out.println(compte1);
+    }
+}
